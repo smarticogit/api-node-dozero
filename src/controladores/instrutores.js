9720556b5ed42119ce7d2401411dc1609299cc87
daniel.silveira <daniel.silveira@ifood.com.br>
2022-09-08 20:47:13 -0300

    Obter por ID

--- src/controladores/instrutores.js
+++ src/controladores/instrutores.js
@@ -4,4 +4,20 @@ const listarInstrutores = (req, res) => {
     return res.json(instrutores);
 };
 
-module.exports = { listarInstrutores };
+const obterInstrutor = (req, res) => {
+    const { id } = req.params;
+
+    const instrutor = instrutores.find(instrutor => {
+        return instrutor.id === Number(id);
+    });
+
+    if(!instrutor) {
+        return res.status(404).json({ mensagem: 'Instrutor não encontrado!'});
+    }
+    return res.status(200).json(instrutor);
+};
+
+module.exports = { 
+    listarInstrutores,
+    obterInstrutor
+};
